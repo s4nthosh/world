ec27ba65415fc03f738886ac9955db77dd0e8ecc
s4nthosh <santhoshshr43@gmail.com>
2023-02-02 15:14:45 +0530

    updated

--- world/index.js
+++ world/index.js
@@ -4,4 +4,12 @@ tl.to(".text",{ y: "0%",duration: 1, stagger:0.25});
 tl.to(".slider",{y:"-100%",duration: 1.5,delay:0.5})
 tl.to(".intro",{y:"-100%",duration:1},"-=1.5");
 tl.fromTo("video",{opacity:0},{opacity:1,duration:1},"-=.5")
-tl.fromTo("nav",{opacity:0},{opacity:1,duration:1})
+tl.fromTo("nav",{opacity:0},{opacity:1,duration:1});
+
+const hamburger = document.querySelector(".hamburger");
+const navlinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener("click",() => {
+    navlinks.classList.toggle("open");
+});
